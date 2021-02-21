d4d73bb9d283a3565874b09e70f90afa537680fa
Lucas Nascimento <lucasnascimentoribeiro13@gmail.com>
2021-02-21 16:41:12 -0300

    Add sidebar responsive rules

---
+++ assets/js/sidebar.js
@@ -0,0 +1,14 @@
+const links = document.querySelectorAll(".sidebar .sidebar-item-dropdown a");
+links.forEach((link) => {
+	link.onclick = (event) => {
+		event.preventDefault();
+		link.parentElement.classList.toggle("active");
+	};
+});
+
+const toggleSidebar = () => {
+	document.body.classList.toggle("sidebar-active");
+};
+
+const menu = document.querySelector("#sidebar-toggler");
+menu.onclick = toggleSidebar;
